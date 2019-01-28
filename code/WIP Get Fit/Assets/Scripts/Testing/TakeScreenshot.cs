daa4a16b43ef43d7d6655c15c3001ebbea640b99
mapluisch <martin.pluisch@icloud.com>
2019-01-29 00:26:34 +0100

    added final presentation and screenshots

---
+++ code/WIP Get Fit/Assets/Scripts/Testing/TakeScreenshot.cs
@@ -0,0 +1,18 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TakeScreenshot : MonoBehaviour
+{
+
+	[MenuItem("Tools/Take Screenshot")]
+	static public void OnTakeScreenshot()
+	{
+
+		ScreenCapture.CaptureScreenshot(EditorUtility.SaveFilePanel("Save Screenshot As", "", "", "png"));
+	}
+
+}
+#endif
--- code/WIP Get Fit/Assets/Scripts/Testing/TestData.cs
+++ code/WIP Get Fit/Assets/Scripts/Testing/TestData.cs
@@ -6,18 +6,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class TestData : MonoBehaviour {
-	public bool OVERRIDE_FIRSTSTART = false, INJECT_TESTDATA = false;
+    public bool OVERRIDE_FIRSTSTART = false, INJECT_TESTDATA = false;
 
-	void Start () {
-		if (OVERRIDE_FIRSTSTART) GameManager.instance.intro.SetActive (true);
-		if (INJECT_TESTDATA) {
-			GameManager.instance.workoutHistory.Add (DateTime.Parse ("10/12/2018", new CultureInfo ("de-DE", true)), new List<WorkoutSession> (new WorkoutSession[] { new WorkoutSession (21, 360f, 360f, 75) }));
+    void Start() {
+        if (OVERRIDE_FIRSTSTART) GameManager.instance.intro.SetActive(true);
+        if (INJECT_TESTDATA) {
+            GameManager.instance.workoutHistory.Add(DateTime.Parse("10/12/2018", new CultureInfo("de-DE", true)), new List<WorkoutSession>(new WorkoutSession[] { new WorkoutSession(21, 360f, 360f, 75) }));
 
-			GameManager.instance.workoutHistory.Add (DateTime.Parse ("9/12/2018", new CultureInfo ("de-DE", true)), new List<WorkoutSession> (new WorkoutSession[] { new WorkoutSession (18, 750f, 750f, 225) }));
+            GameManager.instance.workoutHistory.Add(DateTime.Parse("9/12/2018", new CultureInfo("de-DE", true)), new List<WorkoutSession>(new WorkoutSession[] { new WorkoutSession(18, 750f, 750f, 225) }));
 
-			GameManager.instance.workoutHistory.Add (DateTime.Parse ("8/12/2018", new CultureInfo ("de-DE", true)), new List<WorkoutSession> (new WorkoutSession[] { new WorkoutSession (19, 400f, 400f, 125) }));
+            GameManager.instance.workoutHistory.Add(DateTime.Parse("8/01/2019", new CultureInfo("de-DE", true)), new List<WorkoutSession>(new WorkoutSession[] { new WorkoutSession(19, 400f, 400f, 125) }));
 
-			GameManager.instance.workoutHistory.Add (DateTime.Parse ("6/12/2018", new CultureInfo ("de-DE", true)), new List<WorkoutSession> (new WorkoutSession[] { new WorkoutSession (18, 100f, 100f, 55) }));
-		}
-	}
+            GameManager.instance.workoutHistory.Add(DateTime.Parse("6/12/2018", new CultureInfo("de-DE", true)), new List<WorkoutSession>(new WorkoutSession[] { new WorkoutSession(18, 100f, 100f, 55) }));
+        }
+    }
 }
